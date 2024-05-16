96a41c897a942f52d528dfa638960e41a4e59d08
Brian  Hess <brianhess@durhambrands.com>
2024-05-16 09:51:15 -0600

    Added Flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,15 @@
 import type { Config } from "tailwindcss";
+import flowbite from "flowbite-react/tailwind";
+
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/lib/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -15,6 +20,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin()
+  ],
 };
 export default config;
